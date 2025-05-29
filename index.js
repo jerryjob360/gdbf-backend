982352378d45742e1b93e3849d929e5218152b2c
Jerry Job <jerryjob360@gmail.com>
2025-05-30 01:33:47 +0300

    Image upload to live page

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors({ 
-    origin: 'https://gdbf-frontend-cahpskhk5-jerry-jobs-projects.vercel.app' }));
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            'https://gdbf.or.tz',
+            'https://gdbf-frontend.vercel.app',
+            'https://gdbf-frontend-cahpskhk5-jerry-jobs-projects.vercel.app'
+        ];
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 const db = require('./models');
 // db.sequelize.sync({ force:true });
@@ -22,7 +35,7 @@ const activityRouter = require('./routes/Activity');
 const { FORCE } = require('sequelize/lib/index-hints');
 app.use("/activity", activityRouter);
 
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(Path2D.join(__dirname,  'uploads')));
 
 db.sequelize.sync().then(() => {     //The 'alter: true' should be removed during production.
     app.listen(PORT, () => {
