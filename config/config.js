3b927a011f00f68a4e00f8332bdd93297be94981
Jerry Job <jerryjob360@gmail.com>
2025-05-29 09:24:00 +0300

    New file (.gitignore

---
+++ config/config.js
@@ -0,0 +1,26 @@
+const config = require(__dirname + '/../config/config.js')[env];
+
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'sbecdb',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
