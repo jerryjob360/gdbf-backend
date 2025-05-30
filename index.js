8ed6c569ac13d1d21fbd251b4a44231b461e6c7e
Jerry Job <jerryjob360@gmail.com>
2025-05-30 14:57:37 +0300

    DNS Activated!

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.use(cors({
     origin: (origin, callback) => {
         const allowedOrigins = [
             // 'https://gdbf-frontend-cahpskhk5-jerry-jobs-projects.vercel.app',
-            // // 'https://gdbf.or.tz',
+            'https://gdbf.or.tz',
+            'https://www.gdbf.or.tz'
             // // 'https://gdbf-frontend.vercel.app'
             // 'https://gdbf-frontend-c4yp39b47-jerry-jobs-projects.vercel.app',
             // 'https://gdbf-frontend-c4yp39b47-jerry-jobs-projects.vercel.app'
-            'https://gdbf-frontend-g4cdgiqah-jerry-jobs-projects.vercel.app'
+            // 'https://gdbf-frontend-g4cdgiqah-jerry-jobs-projects.vercel.app'
         ];
         if (allowedOrigins.includes(origin)) {
             return callback(null, true);
