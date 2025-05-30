364273a626f785ccb3a28f2042a0821493ac7fbb
Jerry Job <jerryjob360@gmail.com>
2025-05-30 16:21:07 +0300

    Attempting to upload events with images via Cloudinary

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -7,7 +7,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
     // cloud_name: 'dqswh5cp4',
     // api_key: '225431548792421',
-    // api_secret: '6BHKLVPv7KWf1coUFZ774ITCPhc,'
+    // api_secret: '6BHKLVPv7KWf1coUFZ774ITCPhc'
 });
 
 const storage = new CloudinaryStorage({
