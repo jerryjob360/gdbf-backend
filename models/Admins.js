aab07c639a4092908151502fccee9a4d32f5da25
Jerry Job <jerryjob360@gmail.com>
2026-03-17 15:04:38 +0300

    Removing timesptamps to sequelize

--- models/Admins.js
+++ models/Admins.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
     }, {
-        timestamps: true,
+        timestamps: false,
     });
 
     return Admins;
